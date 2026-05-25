d6af05e82807d7f78756fb7ac8590a038eb6dc4e
Edmund Valdoz <edmundmarievaldoz@gmail.com>
2026-05-25 15:30:54 +0100

    practice task

---
+++ 03_07/script.js
@@ -0,0 +1,11 @@
+//create an object called "cellphone" with the following properties: brand, model, color, price, RAM, storage, and is5GCapable. Assign appropriate values to each property.
+
+const cellphone = {
+  brand: "Apple",
+  model: "iPhone 12",
+  color: "Black",
+  price: 799,
+  RAM: "4GB",
+  storage: "64GB",
+  is5GCapable: true,
+};
